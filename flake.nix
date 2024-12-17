f2175f91b9f3fdfe953011bad20f5e01abacb549
Elizabeth <checksum@pyro.host>
2024-12-16 18:10:55 -0600

    build: working nix build

--- flake.nix
+++ flake.nix
@@ -5,161 +5,113 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    advisory-db = {
-      url = "github:rustsec/advisory-db";
-      flake = false;
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    crane,
-    flake-utils,
-    advisory-db,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (pkgs) lib;
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      craneLib = crane.mkLib pkgs;
-      src = craneLib.cleanCargoSource ./.;
+        craneLib = crane.mkLib pkgs;
 
-      commonArgs = {
-        inherit src;
-        strictDeps = true;
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
 
-        buildInputs =
-          [
-            pkgs.openssl
+          nativeBuildInputs = [
             pkgs.pkg-config
-          ]
-          ++ lib.optionals pkgs.stdenv.isDarwin [
+          ];
+
+          buildInputs = [
+            pkgs.openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.libiconv
             pkgs.darwin.apple_sdk.frameworks.Security
             pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           ];
 
-        DISCORD_TOKEN = ""; # Token will be provided via environment
-      };
-
-      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-      pyrobot = craneLib.buildPackage (commonArgs
-        // {
-          inherit cargoArtifacts;
-        });
-
-      # NixOS module for the Pyrobot service
-      pyrobotModule = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: {
-        options.services.pyrobot = {
-          enable = lib.mkEnableOption "Pyro Discord Bot";
-          tokenFile = lib.mkOption {
-            type = lib.types.path;
-            description = "File containing the Discord bot token";
-          };
-          user = lib.mkOption {
-            type = lib.types.str;
-            default = "pyrobot";
-            description = "User account under which the bot runs";
-          };
-          group = lib.mkOption {
-            type = lib.types.str;
-            default = "pyrobot";
-            description = "Group under which the bot runs";
-          };
+          DISCORD_TOKEN = "";
         };
 
-        config = lib.mkIf config.services.pyrobot.enable {
-          users.users.${config.services.pyrobot.user} = {
-            isSystemUser = true;
-            group = config.services.pyrobot.group;
-            description = "Pyrobot Discord bot service user";
+        pyrobot = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
+
+        pyrobotModule = { config, lib, pkgs, ... }: {
+          options.services.pyrobot = {
+            enable = lib.mkEnableOption "Pyro Discord Bot";
+            token = lib.mkOption {
+              type = lib.types.str;
+              description = "Discord bot token";
+            };
+            user = lib.mkOption {
+              type = lib.types.str;
+              default = "pyrobot";
+              description = "User account under which the bot runs";
+            };
+            group = lib.mkOption {
+              type = lib.types.str;
+              default = "pyrobot";
+              description = "Group under which the bot runs";
+            };
           };
 
-          users.groups.${config.services.pyrobot.group} = {};
+          config = lib.mkIf config.services.pyrobot.enable {
+            users.users.${config.services.pyrobot.user} = {
+              isSystemUser = true;
+              group = config.services.pyrobot.group;
+              description = "Pyro Discord bot service user";
+            };
 
-          systemd.services.pyrobot = {
-            description = "Pyrobot Discord Bot";
-            wantedBy = ["multi-user.target"];
-            after = ["network-online.target"];
-            wants = ["network-online.target"];
+            users.groups.${config.services.pyrobot.group} = {};
 
-            serviceConfig = {
-              Type = "simple";
-              User = config.services.pyrobot.user;
-              Group = config.services.pyrobot.group;
-              ExecStart = "${pyrobot}/bin/pyrobot";
-              Restart = "always";
-              RestartSec = "30s";
+            systemd.services.pyrobot = {
+              description = "Pyro Discord Bot";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
 
-              # Security hardening
-              NoNewPrivileges = true;
-              PrivateTmp = true;
-              PrivateDevices = true;
-              ProtectSystem = "strict";
-              ProtectHome = true;
-              ReadOnlyDirectories = "/";
-              ReadWritePaths = [];
-              PrivateUsers = true;
-
-              # Environment setup
-              EnvironmentFile = config.services.pyrobot.tokenFile;
+              serviceConfig = {
+                Type = "simple";
+                User = config.services.pyrobot.user;
+                Group = config.services.pyrobot.group;
+                ExecStart = "${pyrobot}/bin/pyrobot";
+                Restart = "always";
+                RestartSec = "30s";
+                NoNewPrivileges = true;
+                PrivateTmp = true;
+                PrivateDevices = true;
+                ProtectSystem = "strict";
+                ProtectHome = true;
+                ReadOnlyDirectories = "/";
+                PrivateUsers = true;
+                Environment = "DISCORD_TOKEN=${config.services.pyrobot.token}";
+              };
             };
           };
         };
-      };
-    in {
-      checks = {
-        inherit pyrobot;
-
-        pyrobot-clippy = craneLib.cargoClippy (commonArgs
-          // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
-
-        pyrobot-fmt = craneLib.cargoFmt {
-          inherit src;
+      in
+      {
+        checks = {
+          inherit pyrobot;
         };
 
-        pyrobot-audit = craneLib.cargoAudit {
-          inherit src advisory-db;
+        packages.default = pyrobot;
+
+        nixosModules.default = pyrobotModule;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = pyrobot;
         };
 
-        pyrobot-nextest = craneLib.cargoNextest (commonArgs
-          // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
-      };
-
-      packages = {
-        default = pyrobot;
-      };
-
-      nixosModules.default = pyrobotModule;
-
-      apps.default = flake-utils.lib.mkApp {
-        drv = pyrobot;
-      };
-
-      devShells.default = craneLib.devShell {
-        checks = self.checks.${system};
-
-        packages = with pkgs; [
-          pkg-config
-          openssl
-          rust-analyzer
-          alejandra
-        ];
-      };
-    });
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [
+            pkg-config
+            openssl
+            rust-analyzer
+            alejandra
+          ];
+        };
+      });
 }
